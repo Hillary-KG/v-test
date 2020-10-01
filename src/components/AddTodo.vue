be3a37cadbbf9c8260ba59a2ce0dc49137fd0504
Hillary <hillary.kamau@eadatahandlers.co.ke>
2020-10-01 17:06:47 +0300

    added to do form

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <input type="text" v-model="title" name="title" placeholder="Add Todo ..." />
+    <input type="submit" value="Submit" class="btn" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data(){
+      return{
+          title:''
+      }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
